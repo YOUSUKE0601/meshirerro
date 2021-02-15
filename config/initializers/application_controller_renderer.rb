f52ff14d92b92463c5a03ca71afb1fcf2f216429
YOUSUKE0601 <tyousuke0601@gmail.com>
2021-02-15 10:16:20 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5f1592e5a3a0590645b032b3bb92599d26e83e83f2d15db58255d828ef8c2e1aec0d4a8f475d934b63b2e6be5cc6c333eccd87809985761823856b08d0db41e2'
